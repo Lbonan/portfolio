dc6aef91c55aeebeabb7de6245ac5bd79f6e4847
Lucas Bonan <lucasbonan15@gmail.com>
2024-09-22 16:04:21 -0300

    construindo a estrutura

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/lbonan.png" alt="foto de avatar do github" />
+    <Titulo>Lucas Bonan</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,14 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim ipsum eum
+      quidem veritatis repellat laborum atque blanditiis alias saepe qui officia
+      minima earum explicabo, sed eligendi quis amet odio reiciendis!
+    </p>
+  </section>
+)
+
+export default Sobre
