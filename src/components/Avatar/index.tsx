44a6d04a95b18721fb815683a6f9d9c8f9230c97
Lucas Bonan <lucasbonan15@gmail.com>
2024-09-23 17:57:15 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,10 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil
+    src="https://github.com/lbonan.png"
+    alt="foto de avatar do github"
+  />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SiderbarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/lbonan.png" alt="foto de avatar do github" />
-    <Titulo fontSize={20}>Lucas Bonan</Titulo>
+    <SiderbarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Lucas Bonan</Titulo>
+      <Paragrafo tipo="secundaria" fontSize={16}>
+        lbonan
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor full stack
+      </Descricao>
+      <BotaoTema>Trocar de tema</BotaoTema>
+    </SiderbarContainer>
   </aside>
 )
 
